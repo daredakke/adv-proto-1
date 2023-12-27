71ad8943f7e46aa3c92ef2d08bb7772b696c640a
daredakke <77235881+daredakke@users.noreply.github.com>
2023-12-27 21:04:38 +0000

    Add dialogue box UI component

---
+++ scripts/dialogue_box.gd
@@ -0,0 +1,6 @@
+class_name DialogueBox
+extends Control
+
+
+func _ready() -> void:
+	self.visible = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,15 +4,22 @@ extends CharacterBody2D
 signal player_moving(direction: float)
 signal player_stopped
 
-const SPEED: float = 500.0
-const INTERACTION_ORIGIN_OFFSET: int = 60
+enum State {
+	EXPLORATION,
+	TALKING
+}
 
+const SPEED: float = 500.0
+const INTERACTION_ORIGIN_OFFSET: int = 50
+
+# Origin point for NPC interaction is offset left or right based on player's facing
 @onready var interaction_origin: Marker2D = %InteractionOrigin
 
 @export var interaction_range: float = 125
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player_state: State = State.EXPLORATION
 
 
 func _ready() -> void:
@@ -20,27 +27,32 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	# Find closest NPC in range to select for interaction
-	var closest_npc = null
-	var closest_npc_distance: float = 0
-	
-	for npc in get_tree().get_nodes_in_group("npc"):
-		npc.is_selected(false)
+	if player_state == State.EXPLORATION:
+		# Find closest NPC in range to select for interaction
+		var closest_npc = null
+		var closest_npc_distance: float = 0
 		
-		var current_npc_distance = interaction_origin.global_position.distance_to(npc.global_position)
+		for npc in get_tree().get_nodes_in_group("npc"):
+			npc.is_selected(false)
+			
+			var current_npc_distance = interaction_origin.global_position.distance_to(npc.global_position)
+			
+			if current_npc_distance > interaction_range:
+				continue
+			
+			if !closest_npc or current_npc_distance < closest_npc_distance:
+				closest_npc = npc
+				closest_npc_distance = current_npc_distance
 		
-		if current_npc_distance > interaction_range:
-			continue
+		# Show visual indication that an NPC can be interacted with
+		if closest_npc:
+			closest_npc.is_selected(true)
 		
-		if !closest_npc or current_npc_distance < closest_npc_distance:
-			closest_npc = npc
-			closest_npc_distance = current_npc_distance
-	
-	if closest_npc:
-		closest_npc.is_selected(true)
-	
-	if Input.is_action_just_pressed("action") and closest_npc:
-		print(closest_npc.npc_name)
+		if Input.is_action_just_pressed("action") and closest_npc:
+			print(closest_npc.npc_name)
+		
+	elif player_state == State.TALKING:
+		pass
 
 
 func _physics_process(delta: float) -> void:
@@ -49,7 +61,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction: float = Input.get_axis("left", "right")
 	
 	if direction:
