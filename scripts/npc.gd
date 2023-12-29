7b2858f49c411ccfb0c0828d982c4849d572883b
daredakke <77235881+daredakke@users.noreply.github.com>
2023-12-29 20:04:50 +0000

    Moving NPCs can aim at a position to move to

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -5,16 +5,61 @@ const SELECTED_ARROW_GAP: int = 24
 
 @onready var npc_sprite: Sprite2D = $NPCSprite
 @onready var selected_arrow: Sprite2D = $SelectedArrow
+@onready var wait_timer: Timer = $WaitTimer
+@onready var marker_nodes: Node2D = $Markers
 
 @export var npc_texture: Texture2D
 @export var npc_id: NpcData.ID
+
+@export_category("NPC Movement")
+@export var can_move: bool = false
+@export var is_grounded: bool = true
+@export var patrol: bool = true
+@export_range(0, 1000) var speed: float = 200
+@export_range(0, 60) var pause_duration: float
+
+var npc_gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var markers: Array[Vector2]
+var next_marker_index: int = 0
+var direction_to_marker: Vector2
 
 
 func _ready() -> void:
 	is_selected(false)
 	
+	wait_timer.wait_time = pause_duration
+	
+	if can_move and marker_nodes.get_children():
+		for marker in marker_nodes.get_children():
+			markers.append(marker.global_position)
+		
+		direction_to_marker = get_direction_to_next_coord()
+		print(npc_id, direction_to_marker)
+
 	if npc_texture:
 		npc_sprite.texture = npc_texture
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func get_direction_to_next_coord() -> Vector2:
+	if markers.size() > 0 and next_marker_index < markers.size():
+		# Get vector pointing towards target position
+		var direction: Vector2 = markers[next_marker_index] - self.global_position
+		direction = direction.normalized()
+
+		# Grounded NPCs only need to move left and right
+		if is_grounded:
+			if direction.x != 0:
+				direction.x = clamp(direction.x * 10, -1, 1)
+			
+			direction.y = 0
+		
+		return direction
+	
+	return Vector2.ZERO
 
 
 func is_selected(state: bool) -> void:
